d0dcc8b7fa03c7838dceac7abd9b76fe670ec609
salimi <salimiiz888@gmail.com>
2025-05-02 23:25:11 +0330

    Reasons to buy section completed

--- my-app/src/component/main/home-page/IndexHomepage.jsx
+++ my-app/src/component/main/home-page/IndexHomepage.jsx
@@ -2,11 +2,13 @@ import HeroSectionHomepage from "./HeroSectionHomepage";
 import 'bootstrap/dist/css/bootstrap.rtl.min.css';
 import './Homepage.css'
 import './ResponsiveHomepage.css'
+import ReasonsToBuySection from "./ReasonsToBuySection";
 
 export default function IndexHomepage(){
     return(
         <div className=""  dir="rtl">
             <HeroSectionHomepage />
+            <ReasonsToBuySection />
         </div>
     )
 }

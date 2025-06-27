911f82103ac6aae78cb508a1cf7c877e938c25cf
Asal Yousefi <asalyousefi8866@gmail.com>
2025-06-27 20:38:34 +0330

    important links created

---
+++ my-app/src/component/Shop/Footer/ImportantLinks.jsx
@@ -0,0 +1,13 @@
+export default function ImportantLinks() {
+    return (
+        <div>
+            <div className="text-white">
+                <h2>لینک های مهم</h2>
+                <ul>
+                    
+                </ul>
+            </div>
+            <div></div>
+        </div>
+    )
+}

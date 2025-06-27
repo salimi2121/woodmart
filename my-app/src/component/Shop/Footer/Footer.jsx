789d01c57bfa5973bc003bab33a024081d362659
Asal Yousefi <asalyousefi8866@gmail.com>
2025-06-27 20:39:16 +0330

    working on footer....

--- my-app/src/component/Shop/Footer/Footer.jsx
+++ my-app/src/component/Shop/Footer/Footer.jsx
@@ -1,27 +1,16 @@
-import { PiPhoneCallFill } from "react-icons/pi";
+import AboutUs from "./AboutUs";
+import ContactUs from "./ContactUs";
 
 export default function Footer() {
-    return (
-        <footer>
-            <div className="footer-container mx-2">
-                <div className="orange-line mx-2"></div>
-                <div className="footer-content">
-                    <div className="contact-us text-white">
-                        <div>
-                            <h4>مسیر های ارتباطی</h4>
-                            <div>
-                                <div className="d-flex">
-                                    <div className="icon p-3"><PiPhoneCallFill /></div>
-                                    <div className="mx-2">
-                                        <span>شماره تماس</span>
-                                        <p>۰۹۲</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </footer>
-    )
+  return (
+    <footer>
+      <div className="footer-container mx-2">
+        <div className="orange-line mx-2"></div>
+        <div className="footer-content p-4">
+          <ContactUs />
+          <AboutUs />
+        </div>
+      </div>
+    </footer>
+  );
 }

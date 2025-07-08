a47393ec493f21487fcb06d80410300c2896e0ed
Asal Yousefi <asalyousefi8866@gmail.com>
2025-07-08 17:03:57 +0330

    cart almost completed

--- my-app/src/component/Shop/SideMenu/Cart.jsx
+++ my-app/src/component/Shop/SideMenu/Cart.jsx
@@ -1,6 +1,8 @@
 import { useContext } from "react";
 import SideMenuHeader from "./SideMenuHeader";
 import { CartContext } from "../contexts/CartContext";
+import { MdOutlineRemoveShoppingCart } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 export default function Cart() {
 
@@ -9,9 +11,15 @@ export default function Cart() {
     return (
         <div>
             <SideMenuHeader title="سبد خرید" />
-            {/* {cart.length === 0 ? (
-                <div></div>
-            ) :} */}
+            {cart.length === 0 && (
+                <div className="d-flex flex-column align-items-center">
+                    <div className="d-flex flex-column align-items-center mb-3 mt-4">
+                        <MdOutlineRemoveShoppingCart className="empty-cart-icon mb-2" />
+                        <span className="fw-bold">هیچ محصولی در سبد خرید نیست.</span>
+                    </div>
+                    <Link to="/" className="return-btn fw-500 bg-orange rounded-5 text-white my-3">بازگشت به فروشگاه</Link>
+                </div>
+            )}
         </div>
     )
 }
--- my-app/src/component/Shop/SideMenu/SideMenuHeader.jsx
+++ my-app/src/component/Shop/SideMenu/SideMenuHeader.jsx
@@ -9,8 +9,8 @@ export default function SideMenuHeader(props) {
 
   return (
     <div className="sidemenu-header border-1 border-bottom d-flex justify-content-between px-3 py-4">
-      <h5 className="sidemenu-title">{props.title}</h5>
-      <div className="pointer" onClick={() => setIsOpen(false)}>
+      <h5 className="sidemenu-title fw-bold">{props.title}</h5>
+      <div className="pointer close-btn" onClick={() => setIsOpen(false)}>
         <IoCloseOutline className="fs-3" />
         <span className="fw-bold">بستن</span>
       </div>
